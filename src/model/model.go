6e6a8859825a896b389c1df6daab6dd02cd1d2af
Daniel Imfeld <daniel@danielimfeld.com>
2016-01-28 00:04:20 -0600

    Add model for attachments, and template directory config

--- src/model/model.go
+++ src/model/model.go
@@ -20,6 +20,29 @@ type Config struct {
 
 	WebHookBind string `default:":80"`
 	WebHookKey  string `required:"true"`
+
+	TemplateDir string `default:"./templates"`
+}
+
+type AttachmentField struct {
+	Title string `json:"title"`
+	Value string `json:"value"`
+	Short bool   `json:"short,omitempty"`
+}
+
+type ChatAttachment struct {
+	Fallback   string            `json:"fallback,omitempty"`
+	Color      string            `json:"color,omitempty"`
+	Pretext    string            `json:"pretext,omitempty"`
+	AuthorName string            `json:"author_name,omitempty"`
+	AuthorLink string            `json:"author_link,omitempty"`
+	AuthorIcon string            `json:"author_icon,omitempty"`
+	Title      string            `json:"title,omitempty"`
+	TitleLink  string            `json:"title_link,omitempty"`
+	Text       string            `json:"text,omitempty"`
+	Fields     []AttachmentField `json:"fields,omitempty"`
+	ImageUrl   string            `json:"image_url,omitempty"`
+	ThumbUrl   string            `json:"thumb_url,omitempty"`
 }
 
 // ChatMessage contains information about an incoming or outgoing message.
@@ -31,10 +54,11 @@ type ChatMessage struct {
 	// the @user at the beginning of the message.
 	ToUser string
 	// The name of a channel to send to.
-	Channel  string
-	Title    string
-	Contents string
+	Channel string
+	Text    string
 	// For incoming messages, true if the message is addressed to the bot.
 	// This can either be through a DM channel or by starting with @botuser.
 	ToBot bool
+	// Attachment, if any
+	Attachment *ChatAttachment
 }
