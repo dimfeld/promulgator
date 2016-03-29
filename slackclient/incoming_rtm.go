2f72bcbe7292fcb851f82b5520a9cddebde290df
Daniel Imfeld <daniel@danielimfeld.com>
2016-03-28 23:29:48 -0500

    Start of RTM client

--- slackclient/incoming_rtm.go
+++ slackclient/incoming_rtm.go
@@ -4,12 +4,45 @@ import (
 	"sync"
 
 	"github.com/nlopes/slack"
+	"github.com/spacemonkeygo/spacelog"
 
 	"github.com/dimfeld/promulgator/commandrouter"
 	"github.com/dimfeld/promulgator/model"
 )
 
+var rtmLogger *spacelog.Logger
+
+func routeMessage(api *slack.Client, commandrouter *commandrouter.Router, msg *slack.MessageEvent) {
+
+}
+
 func StartIncomingRTM(wg *sync.WaitGroup, config *model.Config, api *slack.Client,
 	commandrouter *commandrouter.Router, done chan struct{}) {
+
+	rtmLogger = spacelog.GetLoggerNamed("slack-rtm")
+
+	// wg.Add(1)
+	//
+	// go (func() {
+	// 	rtm := api.NewRTM()
+	// 	go rtm.ManageConnection()
+	//
+	// 	for {
+	// 		select {
+	// 		case msg := <-rtm.IncomingEvents:
+	// 			if ev, ok := msg.Data.(*slack.MessageEvent); ok {
+	// 				// Handle the message
+	// 				go routeMessage(api, commandrouter, ev)
+	// 			}
+	// 		case <-done:
+	// 			err := rtm.Disconnect()
+	// 			if err != nil {
+	// 				rtmLogger.Errorf("Shutting down Slack RTM: %s", err.Error())
+	// 			}
+	// 			wg.Done()
+	// 			return
+	// 		}
+	// 	}
+	// })()
 
 }
